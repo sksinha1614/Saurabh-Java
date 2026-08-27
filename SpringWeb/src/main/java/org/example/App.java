b5160153e31d7af1a3cb84a862b65be32dd4e4f5
sksinha1614 <sksinha1614@gmail.com>
2026-08-27 16:26:09 +0530

    servlet project

---
+++ SpringWeb/src/main/java/org/example/App.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class App 
+{
+    public static void main( String[] args ) throws LifecycleException {
+        System.out.println( "Hello World!" );
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        tomcat.getConnector();
+
+        Context context = tomcat.addContext("", null);
+
+        Tomcat.addServlet(context, "HelloServlet", new HelloServlet());
+        context.addServletMappingDecoded("/hello", "HelloServlet");
+
+        tomcat.start();
+
+        System.out.println("Tomcat started on port 8080");
+
+        tomcat.getServer().await();
+    }
+}
---
+++ SpringWeb/src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        System.out.println("In Service");
+
+        res.setContentType("text/html");
+
+        PrintWriter out=res.getWriter();
+        out.println("<h1>HELLO WORLD</h1>");
+    }
+}
