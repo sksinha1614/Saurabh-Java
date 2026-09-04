715109c3ba55e45ff314104bba7bbcd68ba6df64
sksinha1614 <sksinha1614@gmail.com>
2026-09-04 22:51:02 +0530

    used Spring Data JPA

---
+++ JobAppRestWithJPA/src/main/java/com/example/JobAppRest/JobRestController.java
@@ -0,0 +1,49 @@
+package com.example.JobAppRest;
+
+
+import com.example.JobAppRest.model.JobPost;
+import com.example.JobAppRest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class JobRestController {
+
+    @Autowired
+    private JobService service;
+
+    @GetMapping("jobPosts")
+    public List<JobPost>getAllJobs(){
+        return service.getAllJobs();
+    }
+
+    @GetMapping("jobPost/{postId}")
+    public JobPost getJob(@PathVariable("postId") int postId){
+        return service.getJob(postId);
+    }
+
+    @PostMapping("/jobPost")
+    public JobPost addJob(@RequestBody JobPost jobPost){
+        service.addJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
+
+    @PutMapping("jobPost/{postId}")
+    public String deleteJob(@PathVariable int postId){
+        service.deleteJob(postId);
+        return "Deleted";
+    }
+
+    @GetMapping("load")
+    public String loadData(){
+        service.load();
+        return "success";
+    }
+
+    @GetMapping("jobPosts/keyword/{keyword}")
+    public List<JobPost>searchByKeyword(@PathVariable("keyword") String keyword){
+        return service.search(keyword);
+    }
+}
